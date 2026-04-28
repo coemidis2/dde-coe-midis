9e7957bfba781194080f3cf1a59fd39009300a70
coemidis2 <coemidis@midis.gob.pe>
2026-04-28 14:48:04 -0500

    Versión 27

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// ================= VERSION 17 24/04/2026 - 14:15 HRS · FIX ROLES SIN EVALUADOR =================
+// ================= VERSION 27 24/04/2026 - 14:15 HRS · FIX ROLES SIN EVALUADOR =================
 const API_BASE = window.location.origin + '/api';
 const LS_USERS = 'dee_matriz_usuarios_v17';
 
