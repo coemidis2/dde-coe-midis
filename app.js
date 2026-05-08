4d4c931928e8e6cd9bb32465dc79c24168115d0f
coemidis2 <coemidis@midis.gob.pe>
2026-05-08 12:27:03 -0500

    Versión 53

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// ================= VERSION 52 FIX LOGIN USUARIOS LOCALES =================
+// ================= VERSION 53 FIX LOGIN USUARIOS LOCALES =================
 const API_BASE = window.location.origin + '/api';
 
 let state = {
